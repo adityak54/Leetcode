6bcec92b82ad3f0f073cc75b79ced8264e8f6000
Aditya Kumar <97836516+adityak54@users.noreply.github.com>
2024-03-30 04:33:15 +0530

    Time: 141 ms (52.58%), Space: 13.2 MB (75.29%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -1,23 +1,33 @@
 class Solution {
-private:
-    int dp[600][101][101];
 public:
-    int f(int i,int len,int n,int m,vector<string> &strs){
-        if(i==len) return 0;
-        if(n==0 && m==0) return 0;
-        if(dp[i][n][m]!=-1) return dp[i][n][m];
-        int take = 0, notTake = 0;
-        int cnt0 = 0, cnt1 = 0;
-        for(int j=0;j<strs[i].size();j++){
-            strs[i][j]=='0'?cnt0++:cnt1++;
-        }
-        if(cnt0<=m && cnt1<=n) take = 1+f(i+1,len,n-cnt1,m-cnt0,strs);
-        notTake = f(i+1,len,n,m,strs);
-        return dp[i][n][m]=max(take,notTake);
-    }
     int findMaxForm(vector<string>& strs, int m, int n) {
         int len = strs.size();
-        memset(dp,-1,sizeof(dp));
-        return f(0,len,n,m,strs);
+        vector<pair<int,int>> cnt;
+        for(int i=0;i<len;i++){
+            int cnt0 = 0, cnt1 = 0;
+            for(int j=0;j<strs[i].size();j++){
+                strs[i][j]=='0'?cnt0++:cnt1++;
+            }
+            cnt.push_back({cnt0,cnt1});
+        }
+        
+        vector<vector<int>> curr(m+1,vector<int>(n+1,0));
+        vector<vector<int>> ahead(m+1,vector<int>(n+1,0));
+
+        for(int i=len-1;i>=0;i--){
+            for(int j=0;j<=m;j++){
+                for(int k=0;k<=n;k++){
+                    int take = 0, notTake = 0;
+                    int cnt0 = cnt[i].first, cnt1 = cnt[i].second;
+                    if(cnt0<=j && cnt1<=k){
+                        take = 1+ahead[j-cnt0][k-cnt1];
+                    }
+                    notTake = ahead[j][k];
+                    curr[j][k] = max(take,notTake);
+                }
+            }
+            ahead = curr;
+        }
+        return curr[m][n];
     }
 };
