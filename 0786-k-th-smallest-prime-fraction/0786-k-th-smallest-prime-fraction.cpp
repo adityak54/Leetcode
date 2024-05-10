a38c39c19e261455b3c1ceb4e8af649c1997d9f8
Aditya Kumar <97836516+adityak54@users.noreply.github.com>
2024-05-10 23:52:10 +0530

    Time: 238 ms (67.12%), Space: 12.4 MB (78.34%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
@@ -1,18 +1,22 @@
+#define ppi pair<double,pair<int,int>>
 class Solution {
 public:
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<int> ans;
-        priority_queue<pair<double,pair<int,int>>> pq;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                double fraction = double(arr[i])/double(arr[j]);
-                pq.push({fraction,{arr[i],arr[j]}});
-                if(pq.size()>k) pq.pop();
-            }
+        priority_queue<ppi,vector<ppi>,greater<ppi>> pq;
+        vector<int> ans(2);
+        for(int i=0;i<n-1;i++){
+            pq.push({double(double(arr[i])/double(arr[n-1])),{i,n-1}});
         }
-        ans.push_back(pq.top().second.first);
-        ans.push_back(pq.top().second.second);
+        int req = 1;
+        while(req<k){
+            int i=pq.top().second.first;
+            int j=pq.top().second.second-1;
+            pq.pop();
+            pq.push({double(double(arr[i])/double(arr[j])),{i,j}});
+            req++;
+        }
+        ans[0]=arr[pq.top().second.first], ans[1]=arr[pq.top().second.second];
         return ans;
     }
 };
