108b9e21e66ee2e1f0d13ec5379ca88bc2d8b34d
Aditya Kumar <97836516+adityak54@users.noreply.github.com>
2024-05-20 08:09:05 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (88.36%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -1,19 +1,10 @@
 class Solution {
-    int ans = 0;
 public:
-    void f(int i,int currXor,vector<int> &nums){
-        int n = nums.size();
-        if(i==n){
-            if(currXor!=-1) ans+=currXor;
-            return;
-        }
-        f(i+1,currXor,nums); // notTake
-        if(currXor==-1) currXor = nums[i];
-        else currXor^=nums[i];
-        f(i+1,currXor,nums); // take
-    }
     int subsetXORSum(vector<int>& nums) {
-        f(0,-1,nums);
-        return ans;
+        int ans = 0, n = nums.size();
+        for(int i=0;i<n;i++){
+            ans|=nums[i];
+        }
+        return (ans<<(n-1));
     }
 };
