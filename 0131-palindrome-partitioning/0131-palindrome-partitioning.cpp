1c3c9ed4bdb72ffdbd034b778b7f1ff3a1430488
Aditya Kumar <97836516+adityak54@users.noreply.github.com>
2024-05-22 10:15:32 +0530

    Time: 97 ms (52.03%), Space: 69.8 MB (40.08%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,33 +1,31 @@
 class Solution {
-private:
-    bool isPalin(string &s){
-        int n = s.size();
+public:
+    bool isPalin(string &curr){
+        int n = curr.size();
         for(int i=0;i<n/2;i++){
-            if(s[i]!=s[n-i-1]) return false;
+            if(curr[i]!=curr[n-i-1]) return false;
         }
         return true;
     }
-    void f(int i,string curr,vector<string> &currPartitions,string &s,vector<vector<string>> &ans){
+    void f(int i, string curr, vector<string> &currPart,string &s,vector<vector<string>> &ans){
         int n = s.size();
         if(i==n){
-            if(curr.empty())  ans.push_back(currPartitions);
+            if(curr.empty()) ans.push_back(currPart);
             return;
         }
-        for(int j=i;j<n;j++){
-            curr.push_back(s[j]);
-            if(isPalin(curr)){
-                currPartitions.push_back(curr);
-                f(j+1,"",currPartitions,s,ans);
-                currPartitions.pop_back();
-            }
+        curr.push_back(s[i]);
+        f(i+1,curr,currPart,s,ans);
+        if(isPalin(curr)){
+            currPart.push_back(curr);
+            f(i+1,"",currPart,s,ans);
+            currPart.pop_back();
         }
+        curr.pop_back();
     }
-public:
     vector<vector<string>> partition(string s) {
-        int n = s.size();
-        vector<string> currPartitions;
         vector<vector<string>> ans;
-        f(0,"",currPartitions,s,ans);
+        vector<string> currPart;
+        f(0,"",currPart,s,ans);
         return ans;
     }
 };
