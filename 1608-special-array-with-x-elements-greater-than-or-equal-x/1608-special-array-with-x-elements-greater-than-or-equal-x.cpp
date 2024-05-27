89f567b1cc21abb986ed58f292f0d0b7e4357417
Aditya Kumar <97836516+adityak54@users.noreply.github.com>
2024-05-27 09:18:43 +0530

    Time: 5 ms (33.11%), Space: 10.4 MB (11.26%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
-    int check(int mid, vector<int> &nums){
-        int n = nums.size();
-        int cnt = 0;
-        for(int i=0;i<n;i++){
-            if(nums[i]>=mid) cnt++;
-        }
-        return cnt;
-    }
     int specialArray(vector<int>& nums) {
         int n = nums.size();
-        int start = 1, end = n;
-        while(start<=end){
-            int mid = start-(start-end)/2;
-            int val = check(mid,nums);
-            if(val==mid){
-                return mid;
-            }else if(val>mid){
-                start = mid+1;
-            }else{
-                end = mid-1;
-            }
+        vector<int> freq(n+1);
+        for(int i=0; i<n; i++){
+            freq[min(n,nums[i])]++;
+        }
+        int sum = 0;
+        for(int i=n;i>=1;i--){
+            sum+=freq[i];
+            if(sum==i) return i;
         }
         return -1;
     }
