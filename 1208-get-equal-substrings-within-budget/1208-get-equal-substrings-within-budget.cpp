2f3072d732ae6d171c8ae4f837071e1c75ef0f0f
Aditya Kumar <97836516+adityak54@users.noreply.github.com>
2024-05-28 10:02:28 +0530

    Time: 9 ms (41.73%), Space: 8.7 MB (70.28%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
@@ -3,19 +3,13 @@ public:
     bool check(int mid, string &s, string &t, int maxCost){
         int n = s.size();
         int currCost = 0;
-        string subS="", subT="";
         for(int i=0;i<mid;i++){
-            subS.push_back(s[i]);
-            subT.push_back(t[i]);
             currCost+=abs(s[i]-t[i]);
         }
         int start = 0, end = mid;
         while(end<n){
             if(currCost<=maxCost) return true;
             currCost+=abs(s[end]-t[end])-abs(s[start]-t[start]);
-            subS.erase(0,1), subT.erase(0,1);
-            subS.push_back(s[end]);
-            subT.push_back(t[end]);
             end++, start++;
         }
         return currCost<=maxCost;
