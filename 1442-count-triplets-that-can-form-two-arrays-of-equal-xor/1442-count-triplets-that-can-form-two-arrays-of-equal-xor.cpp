a5bf6c9e1e5faf323db3a7889ca46aa6b0b9f1c6
Aditya Kumar <97836516+adityak54@users.noreply.github.com>
2024-05-30 10:11:43 +0530

    Time: 5 ms (38.75%), Space: 9.1 MB (67.04%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -4,15 +4,10 @@ public:
         int n = arr.size();
         int ans = 0;
         for(int i=0; i<n; i++){
-            int a = arr[i];
+            int currXor = arr[i];
             for(int j=i+1; j<n; j++){
-                int b = arr[j];
-                if(a==b) ans++;
-                for(int k=j+1; k<n; k++){
-                    b = b^arr[k];
-                    if(a==b) ans++;
-                }
-                a = a^arr[j];
+                currXor ^= arr[j];
+                if(currXor==0) ans+=j-i;
             }
         }
         return ans;
