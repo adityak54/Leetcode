e6bd0c909899b9d4c12318bd001ebeef5d7f5e62
Aditya Kumar <97836516+adityak54@users.noreply.github.com>
2024-06-13 14:43:25 +0530

    Time: 114 ms (94.54%), Space: 66 MB (59.95%) - LeetHub

--- 2724-sort-by/2724-sort-by.js
+++ 2724-sort-by/2724-sort-by.js
@@ -6,6 +6,8 @@
 var sortBy = function(arr, fn) {
     arr.sort(function(x,y){
         return fn(x)-fn(y)
+//         positive -> x will come after y
+//         negative -> y will come after x
     })
     return arr;
 };
