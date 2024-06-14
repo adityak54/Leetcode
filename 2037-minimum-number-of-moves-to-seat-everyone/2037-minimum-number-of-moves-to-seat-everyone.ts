5fed94c605c2203a1815993664402914a9992b84
Aditya Kumar <97836516+adityak54@users.noreply.github.com>
2024-06-15 00:23:47 +0530

    Time: 69 ms (38.98%), Space: 52.2 MB (10.17%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.ts
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.ts
@@ -1,16 +1,17 @@
 function minMovesToSeat(seats: number[], students: number[]): number {
-    let freqSeats:number[] = [];
-    let freqStudents:number[] = [];
+    let freqSeats:number[] = Array(101).fill(0);
+    let freqStudents:number[] = Array(101).fill(0);
     for(let i=0; i<students.length; i++){
-        freqSeats[seats[i]] ? freqSeats[seats[i]]++ : freqSeats[seats[i]] = 1;
-        freqStudents[students[i]] ? freqStudents[students[i]]++ : freqStudents[students[i]] = 1;
+        freqSeats[seats[i]]++;
+        freqStudents[students[i]]++;
     }
     let i=0, j=0, ans = 0;
     while(i<101 && j<101){
         if(freqSeats[i] && freqStudents[j]){
-            ans += Math.abs(i-j);
-            freqStudents[j]--;
-            freqSeats[i]--;
+            let moves = Math.min(freqSeats[i], freqStudents[j]);
+            ans += moves*Math.abs(i-j);
+            freqSeats[i]-=moves;
+            freqStudents[j]-=moves;
         }
         else if(freqSeats[i]){
             j++;
